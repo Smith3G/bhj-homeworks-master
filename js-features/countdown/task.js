2a4babb01db9035b18f3cb417c0fc06b28bdd144
Smith3G <mypom87@gmail.com>
2022-06-12 15:57:09 +0300

    countdown

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,24 +1,12 @@
 
 const timerElement = document.getElementById("timer");
 
-// function counts() {
-//         let now = new Date();
-//         gap = date - now;
-
-//         if (gap < 0) {
-//                 alert('Вы победили в конкурсе!')
-//         }
-
-// let days = Math.floor(gap / 1000 / 60 /60 /24);
-// let hours = Math.floor(gap / 1000 / 60 /60) % 24;
-// let minutes = Math.floor(gap / 1000 / 60) % 60;
-// let secs = Math.floor(gap / 1000) % 60;
-// }
+// 
 
 
 
 
-// setInterval(counts, 1000);
+
 function countDown() {
         let timer =+timerElement.textContent;
         timer--;
@@ -27,3 +15,20 @@ function countDown() {
         clearInterval();
 }
 setInterval(countDown, 1000);
+
+
+
+
+function timeCounts() {
+                let now = new Date();
+                gap = date - now;
+        
+                if (gap < 0) {
+                        alert('Вы победили в конкурсе!')
+                }
+        
+        
+        let hours = Math.floor(gap / 1000 / 60 /60) % 24;
+        let minutes = Math.floor(gap / 1000 / 60) % 60;
+        let secs = Math.floor(gap / 1000) % 60;
+        }
