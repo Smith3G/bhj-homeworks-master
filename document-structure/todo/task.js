fc0f0966b1e1ce55de13bbbe70553a04ba6af73f
Smith3G <mypom87@gmail.com>
2022-07-03 18:28:45 +0300

    easy todo

---
+++ document-structure/todo/task.js
@@ -0,0 +1,30 @@
+const taskInput = document.getElementById('task__input');
+const addTaskButton = document.getElementById('tasks__add');
+const taskList = document.getElementById('tasks__list');
+
+
+taskInput.addEventListener('keypress', addTask);
+addTaskButton.addEventListener('click', addTask);
+
+
+function addTask(e) {
+    const task = taskInput;
+
+   if((e.keyCode !== 13) && (this === taskInput)) {
+   return;
+}
+   if (task.value !== '') {
+    taskList.innerHTML += `
+                     <div class="task">
+                         <div class="task__title">
+                        ${task.value}
+                         </div>
+                         <a href="#" class="task__remove" onclick = "this.closest('.task').outerHTML = '';"
+                      >&times;
+                         </a>
+                      </div>`;
+                      task.value = '';
+                      e.preventDefault();
+                      return false;
+   }
+}
